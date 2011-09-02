9181dc1a1e6756391d7ba25094225305b0765fe1
Guillaume Filion <guillaume.filion@gmail.com>
2011-09-02 16:08:47 +0200

    Do not run cron if term is not valid

--- send.py
+++ send.py
@@ -23,24 +23,28 @@ class Sender(webapp.RequestHandler):
 
    def get(self):
 
-      path = None
-      subject = None
 
       # Get all user data.
       data = UserData.gql("WHERE ANCESTOR IS :1", term_key())
 
       for user_data in data:
          term = str(user_data.term)
-         
+
+         # Update last time run.
+         user_data.last_run = datetime.datetime.now()
+         user_data.put()
+
+         # Skip user if term is not valid (or empty incidentally).
+         if not user_data.term_valid:
+            continue
+
+         path = None
+         subject = None
 
          yesterday = datetime.datetime.today() + \
                datetime.timedelta(days=-1)
          date_from = user_data.last_run or yesterday
          date_to = max(yesterday, date_from)
-   
-         # Update last time run.
-         user_data.last_run = datetime.datetime.now()
-         user_data.put()
 
          # Fetch the abstracts.
          try:
@@ -50,7 +54,7 @@ class Sender(webapp.RequestHandler):
                'abstr_list': abstr_list
             }
             path = os.path.join(os.path.dirname(__file__), 'hits.html')
-            subject = "Today on PubMed"
+            subject = "Recently on PubMed"
          except eUtils.PubMedException, error:
             template_values = {
                'pair_list': error.pair_list
@@ -58,24 +62,21 @@ class Sender(webapp.RequestHandler):
             path = os.path.join(os.path.dirname(__file__), 'error.html')
             subject = "Error report: pubcron"
          except eUtils.NoHitException:
-            # Skip mail sending if no hit.
+            # Skip user mail if no hit.
             continue
          except Exception, err:
-            # For all other exceptions, a mail is sent to admin.
+            # For other exceptions, send a mail to amdin.
             mail_admin(str(user_data.user.nickname), "%s" % str(err))
+            # And skip user mail.
+            continue
 
-         try:
-            # Create the email message (with hits or error report).
-            msg = mail.EmailMessage()
-            msg.initialize(to=user_data.user.email(),
-               sender="guillaume.filion@gmail.com",
-               subject=subject, body="Message in HTML format.",
-               html=template.render(path, template_values))
-            msg.send()
-         except Exception, err:
-            # Mail admin if mail sending fails.
-            mail_admin(str(user_data.user.nickname),
-                  "Fail to send mail: %s" % str(err))
+         # Create the email message (with hits or error report).
+         msg = mail.EmailMessage()
+         msg.initialize(to=user_data.user.email(),
+            sender="guillaume.filion@gmail.com",
+            subject=subject, body="Message in HTML format.",
+            html=template.render(path, template_values))
+         msg.send()
 
 
 application = webapp.WSGIApplication([
